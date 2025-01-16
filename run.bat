ef30fb8b95851b37058626f7de3732a779acfa54
pelity2 <thiagopelity@gmail.com>
2025-01-16 12:12:04 -0300

    Update run.bat

--- run.bat
+++ run.bat
@@ -1,3 +1,8 @@
 @echo off
 color a
 py main.py
+git clone https://github.com/Kichi779/Twitch-Viewer-Bot kichiviewerbot
+
+cd kichiviewerbot
+
+pip install -r requirements.txt (Or you can click on install.bat file)
